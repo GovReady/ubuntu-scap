d617816845410f8b119abe37f63e3ab4645cf917
Greg Elin <greg@fotonotes.net>
2014-06-08 08:02:19 -0400

    fix run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,5 @@ oscap xccdf eval \
 	--profile xccdf_ubuntu_profile_default \
 	--cpe ubuntu-cpe.xml \
 	--check-engine-results --oval-results --results results.xml \
-	--report results.html
+	--report results.html \
 	ubuntu-xccdf.xml
